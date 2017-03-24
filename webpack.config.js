5066e5a654877765f2659d1b67c05339de4757ed
Lee Allen <leeallen337@gmail.com>
2017-03-24 16:24:17 -0400

    Adding in Html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: path.resolve(__dirname, 'src') + '/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
 
 const webpackConfig = {
   context: path.resolve(__dirname, 'src'),
@@ -6,7 +13,10 @@ const webpackConfig = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  plugins: [
+    HTMLWebpackPluginConfig
+  ]
 };
 
 module.exports = webpackConfig;
