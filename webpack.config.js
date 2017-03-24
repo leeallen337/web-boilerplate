82348d8dbf2eb0aaadb96448245552c6c23bdc10
Lee Allen <leeallen337@gmail.com>
2017-03-24 16:15:48 -0400

    Adding in more files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
+const path = require('path');
+
 const webpackConfig = {
-  
+  context: path.resolve(__dirname, 'src'),
+  entry: './app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
 };
 
 module.exports = webpackConfig;
